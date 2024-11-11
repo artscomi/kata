d17d402c34d74899ff7aca0f8ffbc76ae01e6519
artscomi <artscomi.web@gmail.com>
2024-11-11 20:06:53 +0100

    feat(): add chips component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import Chips from "./components/Chips";
 import CountryCapitalGame from "./components/CountryCapitalGame";
+
+const chips = Array.from({ length: 30 }, (_, index) => ({
+  label: `chip${index + 1}`,
+}));
 
 function App() {
   return (
     <div className="App">
       <CountryCapitalGame />
+      <div style={{ margin: "40px" }}></div>
+      <Chips chips={chips} maxChips={10}  maxTextLength={4}/>
     </div>
   );
 }
---
+++ src/components/Chips.tsx
@@ -0,0 +1,54 @@
+type ChipListProps = {
+  chips?: { label: string }[];
+  maxChips?: number;
+  maxTextLength?: number;
+};
+
+const Chips = ({ chips, maxChips, maxTextLength }: ChipListProps) => {
+  const displayedChips =
+    maxChips && maxChips > 0 ? chips?.slice(0, maxChips) : chips;
+
+  if (!chips) return null;
+
+  const excessChips =
+    maxChips && maxChips > 0 && maxChips <= chips.length
+      ? chips?.length - maxChips
+      : null;
+
+  return (
+    <>
+      <p>Your chip component</p>
+      <section
+        style={{
+          display: "flex",
+          gap: "8px",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        {displayedChips?.map((chip, index) => {
+          const label =
+            maxTextLength === 0 ? "" : chip.label.slice(0, maxTextLength);
+          return (
+            <div
+              key={index}
+              style={{
+                border: "1px solid #bbbbbb",
+                padding: "4px",
+                borderRadius: "8px",
+              }}
+            >
+              {label}
+            </div>
+          );
+        })}
+        {excessChips ? (
+          <aside>{chips.length - (maxChips ?? chips.length)} more items</aside>
+        ) : null}
+      </section>
+    </>
+  );
+};
+
+export default Chips;
